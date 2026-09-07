0b21eefc8c6fff9d9d9a0f6433f115149d384c04
Erfan Esfahanian <es.erfan95@gmail.com>
2026-09-07 09:57:59 +0200

    chore: drop an unused import

--- src/components/viewer/comments/CommentThread.tsx
+++ src/components/viewer/comments/CommentThread.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { api } from "@/lib/api-client";
-import { cn } from "@/lib/cn";
 import type { HighlightKey } from "@/lib/tokens";
 
 export type CommentRow = {
